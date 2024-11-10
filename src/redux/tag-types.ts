61293764c74becef2f4bc85de7ed7c89427d0f3e
sakir101 <sakirhossainf99@gmail.com>
2024-11-10 11:44:15 +0600

    complete functionality

---
+++ src/redux/tag-types.ts
@@ -0,0 +1,11 @@
+export enum tagTypes {
+    user = "user",
+    admin = "admin",
+    form = "form"
+}
+
+export const tagTypesList = [
+    tagTypes.user,
+    tagTypes.admin,
+    tagTypes.form
+];
